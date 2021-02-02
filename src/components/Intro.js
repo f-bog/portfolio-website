3bee6c9fb518e219149aef64fd404be3ab00cf8d
Fred Bogdanoff <fredericks.webdesigns@gmail.com>
2021-02-02 12:43:40 +1000

    changed intro copy

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -28,7 +28,7 @@ function Intro() {
           </span>
         </h1>
         <p style={{ fontSize: "1.4em" }}>
-          UI/UX focused Front-End Web Developer
+          UI/UX focused Front-End Web Developer.
         </p>
         <div
           role="presentation"
